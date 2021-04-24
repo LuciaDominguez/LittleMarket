18c8904b8d8533021de95564c095344c7fb8bd83
Yarko08 <yassiel_2496@hotmail.com>
2021-04-24 01:49:17 -0500

    Pais Controller 0.1V
    Get all y Create funcional

---
+++ ProyectoNET/LittleMarket/Classes/Core/PaisCore.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LittleMarket.Model;
+using System.Threading.Tasks;
+
+namespace LittleMarket.Classes.Core
+{
+    public class PaisCore
+    {
+        LittleMarketBDContext dbContext;
+        public PaisCore(LittleMarketBDContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public List<Pais> GetAll()
+        {
+            try
+            {
+                //LinQ
+                //Funciones
+                //Lenguaje
+                //SELECT * FROM Usuario
+                var Pais =
+                    (from u in dbContext.Pais
+                  
+                     select u
+                    ).ToList();
+
+                return Pais;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Create(Pais Pais)
+        {
+            try
+            {
+                bool validarUsuario = Validate(Pais);
+
+                if (validarUsuario)
+                {
+
+                    dbContext.Add(Pais);
+                    dbContext.SaveChanges();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public void Delete(Pais Pais)
+        {
+            try
+            {
+                bool validarUsuario = Validate(Pais);
+
+                if (validarUsuario)
+                {
+
+                    dbContext.Add(Pais);
+                    dbContext.SaveChanges();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Validate(Pais Pais)
+        {
+
+            try
+            {
+                if (String.IsNullOrEmpty(Pais.Nombre) )
+                {
+
+                    return false;
+                }
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ ProyectoNET/LittleMarket/Controllers/PaisController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LittleMarket.Classes.Core;
+using LittleMarket.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LittleMarket.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PaisController : ControllerBase
+    {
+
+        private LittleMarketBDContext dbContext;
+
+        public PaisController(LittleMarketBDContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET: api/<PaisController>
+        [HttpGet]
+        public  IActionResult GetALL()
+        {
+            try
+            {
+                var Pais = dbContext.Pais
+
+                        // .Select(x => new { nombre = x.Name,apellido = x.ApellidoPaterno})
+                        .ToList();
+                //var usuarios = (
+                //from s in dbContext.Usuario
+                //join sSCHOLL in dbcontext.otro
+                //where s.Activo == false
+                //select s
+                //).ToList();
+
+                PaisCore PaisCore = new PaisCore(dbContext);
+
+                return Ok(PaisCore.GetAll());
+;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+
+        // POST api/<PaisController>
+        [HttpPost]
+        public IActionResult Create([FromBody] Pais Pais)
+        {
+            try
+            {
+                PaisCore PaisCore = new PaisCore(dbContext);
+
+                PaisCore.Create(Pais);
+                return Ok("Pais Agregado Exitosamente");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)System.Net.HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        public IActionResult Delete2( int id)
+        {
+            try
+            {
+                PaisCore PaisCore = new PaisCore(dbContext);
+
+
+                var pais = dbContext.Pais.First(p => p.Id == id);
+
+
+                dbContext.Pais.Remove(pais);
+                dbContext.SaveChanges();
+
+                return Ok("Pais Eliminado Exitosamente");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)System.Net.HttpStatusCode.InternalServerError, ex);
+            }
+        }
+        // PUT api/<PaisController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PaisController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            try
+            {
+                PaisCore PaisCore = new PaisCore(dbContext);
+
+
+                var pais = dbContext.Pais.First(p => p.Id == id);
+
+
+                dbContext.Pais.Remove(pais);
+                dbContext.SaveChanges();
+
+                return Ok("Pais Eliminado Exitosamente");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)System.Net.HttpStatusCode.InternalServerError, ex);
+            }
+        }
+    }
+}
